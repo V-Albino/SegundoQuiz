0947b8af73b95341419de0a412211aabde9ea666
Victor Brandao <victorbrandao007@gmail.com>
2025-09-11 12:12:58 -0300

    Commit 2 - Randomização das alternativas

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,26 @@ import { useState } from 'react'
 import questions from './questions.json'
 import './App.css'
 
+
+function randomiza(vet){
+  const arr = [...vet]
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
+
 export default function App() {
   const [currentQuestion, setCurrentQuestion] = useState(0)
+  const [options, setOptions] = useState(questions.map(q => randomiza(q.options)))
   const [result, setResult] = useState(Array(questions.length).fill(""))
+  const [history, setHistory] = useState(result)
   const [score, setScore] = useState(0)
 
-  function verifica(i){
+  function verifica(opt){
     const resposta = [...result]
-    if(i === questions[currentQuestion].answer){
+    if(opt === questions[currentQuestion].options[0]){
       resposta[currentQuestion] = "Resposta Correta"
       setScore(score + 1)
     }else{
@@ -19,35 +31,35 @@ export default function App() {
   }
 
   function NextPage(){
-    if(currentQuestion < questions.length - 1){
-      setCurrentQuestion(currentQuestion + 1)
-    }
+    setCurrentQuestion(currentQuestion + 1)
   }
   
   function PreviousPage(){
-    if(currentQuestion > 0){
-      setCurrentQuestion(currentQuestion - 1)
-    }
+    setCurrentQuestion(currentQuestion - 1)
+  }
+  function reset(){
+    //adiciona resultado
+    setCurrentQuestion(0)
+    setResult(Array(questions.length).fill(""))
+    setOptions(questions.map(q => randomiza(q.options)))
+    setScore(0)
   }
 
   return (
     <>
       <h1>Perguntas Gerais</h1>
-
       <div className="">
         <p>
           {currentQuestion + 1} - {questions[currentQuestion].question}
         </p>
-
-        {questions[currentQuestion].options.map((_, index) => (
-          <button onClick={() => verifica(index)} disabled = {result[currentQuestion] !== ""}>
-            {questions[currentQuestion].options[index]}
+        {options[currentQuestion].map((option, _) => (
+          <button key={option} onClick={() => verifica(option)} disabled = {result[currentQuestion] !== ""}>
+            {option}
           </button>
         ))}
-
       </div>
 
-      <div style={{height: "20px"}} >
+      <div style={{height: "20px"}}>
       <p className="read-the-docs"> 
         {result[currentQuestion]} 
       </p>
@@ -55,19 +67,28 @@ export default function App() {
 
       <p>Pontuação: {score} / {questions.length}</p>
       <button onClick={
-        () => PreviousPage()}>Anterior
+        () => PreviousPage()} disabled={currentQuestion===0}>Anterior
       </button>
       <button onClick={
-        () => NextPage()}>Proxima
+        () => NextPage()} disabled={currentQuestion===questions.length-1}>Proxima
       </button>
 
-      <div style={{marginTop:"10px"}}>
+      <div style={{marginTop:"10px", marginBottom:"10px"}}>
       {questions.map((_, index)=>(
         <button key={index} onClick={() => setCurrentQuestion(index)}>
           {index+1}
         </button>
       ))}
       </div>
+      <div>
+        <button onClick={() => reset()}>Finalizar Tentativa</button>
+      </div>
+      <div>
+        <ol>
+          <li>Historico</li>
+        </ol>
+      </div>
     </>
+    
   )
 }
